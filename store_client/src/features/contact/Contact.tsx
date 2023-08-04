9a7960000b07fc4f9c38f3f6f303527b098c220c
gbenroofficial <68187993+gbenroofficial@users.noreply.github.com>
2023-08-04 18:16:13 +0100

    updated Contact pg for redux demo

--- store_client/src/features/contact/Contact.tsx
+++ store_client/src/features/contact/Contact.tsx
@@ -1,11 +1,32 @@
-import React from "react"
-
+import React from "react";
+import { useDispatch, useSelector } from "react-redux/es/exports";
+import { CounterState, DECREMENT_COUNT, INCREMENT_COUNT, decrement, increment } from "./counterReducer";
+import { Button, ButtonGroup, Typography } from "@mui/material";
 const Contact = () => {
+  const dispatch = useDispatch();
+  const { data, title } = useSelector((state: CounterState) => state);
   return (
     <>
-    Contact
+      <Typography variant="h2">{title}</Typography>
+      <Typography variant="h5">here is the data: {data}</Typography>
+      <ButtonGroup>
+        <Button
+          onClick={() => {
+            dispatch(decrement());
+          }}
+        >
+          Decrement
+        </Button>
+        <Button
+          onClick={() => {
+            dispatch(increment());
+          }}
+        >
+          Increment
+        </Button>
+      </ButtonGroup>
     </>
-  )
+  );
 };
 
 export default Contact;
