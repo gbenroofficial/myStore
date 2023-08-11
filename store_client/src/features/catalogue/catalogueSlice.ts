15b32c10d63c75b4773f5a626430c1ad5403e3fc
gbenroofficial <68187993+gbenroofficial@users.noreply.github.com>
2023-08-11 20:47:57 +0100

    getProductAsync and getProductsAsync

--- store_client/src/features/catalogue/catalogueSlice.ts
+++ store_client/src/features/catalogue/catalogueSlice.ts
@@ -1,41 +1,66 @@
-import { createAsyncThunk, createEntityAdapter, createSlice } from "@reduxjs/toolkit";
+import {
+  createAsyncThunk,
+  createEntityAdapter,
+  createSlice,
+} from "@reduxjs/toolkit";
 import { Product } from "../../App/Models/Product";
 import agent from "../../App/api/agent";
+import { RootState } from "../../App/store/configureStore";
 
 const productAdapter = createEntityAdapter<Product>();
 
 export const getProductsAsync = createAsyncThunk<Product[]>(
+  "catalogue/getProductAsync",
+  async () => {
+    try {
+      return await agent.Catalogue.productList();
+    } catch (error) {}
+  }
+);
+
+export const getProductAsync = createAsyncThunk<Product, number>(
     "catalogue/getProductAsync",
-    async() => {
-        return await agent.Catalogue.productList();
+    async (productId) => {
+      try {
+        return await agent.Catalogue.productInfo(productId);
+      } catch (error) {}
     }
-)
+  );
 
 export const catalogueSlice = createSlice({
-    name: "catalogue",
-    initialState: productAdapter.getInitialState(
-        {isProductLoaded: false,
-        status: "idle"}
-    ),
-    reducers:{},
-    extraReducers:builder => {
-        builder.addCase(
-            getProductsAsync.pending, (state) =>{
-                state.status = "pendingGetProducts"
-            }
-        )
-        builder.addCase(
-            getProductsAsync.fulfilled, (state, action) => {
-                productAdapter.setAll(state, action.payload);
-                state.status = "idle";
-                state.isProductLoaded = true;
-            }
-        )
-        builder.addCase(
-            getProductsAsync.rejected, (state)=>{
-                state.status = "idle";
-                state.isProductLoaded = false;
-            }
-        )
-    }
-})
+  name: "catalogue",
+  initialState: productAdapter.getInitialState({
+    isProductsLoaded: false,
+    status: "idle",
+  }),
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getProductsAsync.pending, (state) => {
+      state.status = "pendingGetProducts";
+    });
+    builder.addCase(getProductsAsync.fulfilled, (state, action) => {
+      productAdapter.setAll(state, action.payload);
+      state.status = "idle";
+      state.isProductsLoaded = true;
+    });
+    builder.addCase(getProductsAsync.rejected, (state) => {
+      state.status = "idle";
+      state.isProductsLoaded = false;
+    });
+
+    builder.addCase(getProductAsync.pending, (state) => {
+        state.status = "pendingGetProduct";
+    } );
+    builder.addCase(getProductAsync.fulfilled, (state, action) => {
+        productAdapter.upsertOne(state, action.payload);
+        state.status = "idle";
+    });
+    builder.addCase(getProductAsync.rejected, (state) => {
+        state.status = "idle";
+    })
+  },
+});
+
+export const productSelectors = productAdapter.getSelectors(
+  (state: RootState) => state.catalogue
+);
