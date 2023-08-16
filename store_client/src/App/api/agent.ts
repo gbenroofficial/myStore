2d50070c14860101b0a8e6e19e0fbe601e75370c
gbenroofficial <68187993+gbenroofficial@users.noreply.github.com>
2023-08-16 18:17:39 +0100

    set up get requests to accept params in shorthand

--- store_client/src/App/api/agent.ts
+++ store_client/src/App/api/agent.ts
@@ -46,14 +46,14 @@ axios.interceptors.response.use(
   }
 );
 const requests = {
-  get: (url: string) => axios.get(url).then(responseBody),
+  get: (url: string, params?: URLSearchParams) => axios.get(url, {params}).then(responseBody),
   post: (url: string, body: {}) => axios.post(url).then(responseBody),
   put: (url: string, body: {}) => axios.put(url).then(responseBody),
   delete: (url: string) => axios.delete(url).then(responseBody),
 };
 
 const Catalogue = {
-  productList: () => requests.get("products"),
+  productList: (params: URLSearchParams) => requests.get("products", params),
   productInfo: (id: number) => requests.get(`products/${id}`),
   filters: () => requests.get("products/filters"),
 };
