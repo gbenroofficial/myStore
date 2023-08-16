b548341e9569bbba50a6fb78e14b3eaf4a1330a9
gbenroofficial <68187993+gbenroofficial@users.noreply.github.com>
2023-08-16 13:44:46 +0100

    page redesign (sort, search, filter, pagination)

--- store_client/src/features/catalogue/Catalogue.tsx
+++ store_client/src/features/catalogue/Catalogue.tsx
@@ -7,12 +7,32 @@ import {
   getProductsAsync,
   productSelectors,
 } from "./catalogueSlice";
+import {
+  Box,
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  FormLabel,
+  Grid,
+  Pagination,
+  Paper,
+  Radio,
+  RadioGroup,
+  TextField,
+  Typography,
+} from "@mui/material";
+
+const sortOptions = [
+  { value: "name", label: "Alphabetical" },
+  { value: "priceDesc", label: "Price - High to low" },
+  { value: "price", label: "Price - Low to high" },
+];
 
 const Catalogue = () => {
   const products = useAppSelector(productSelectors.selectAll);
-  const { isProductsLoaded, status, isFiltersLoaded } = useAppSelector(
-    (state) => state.catalogue
-  );
+  const { isProductsLoaded, status, isFiltersLoaded, brands, types } =
+    useAppSelector((state) => state.catalogue);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
@@ -30,9 +50,65 @@ const Catalogue = () => {
   if (status.includes("pending"))
     return <LoadingBox message="Loading Products..." />;
   return (
-    <>
-      <ProductList products={products} />
-    </>
+    <Grid container spacing={4}>
+      <Grid item xs={4} sm={4} md={3} lg={4} xl={3}>
+        <Paper sx={{ mb: 2 }}>
+          <TextField label="Search products" variant="outlined" fullWidth />
+        </Paper>
+        <Paper sx={{ mb: 2, p: 2 }}>
+          <FormControl>
+            <FormLabel id="demo-radio-buttons-group-label">Sort</FormLabel>
+            <RadioGroup
+              aria-labelledby="demo-radio-buttons-group-label"
+              defaultValue={sortOptions[0].value}
+              name="radio-buttons-group"
+            >
+              {sortOptions.map((option) => (
+                <FormControlLabel
+                  value={option.value}
+                  control={<Radio />}
+                  label={option.label}
+                  key={option.value}
+                />
+              ))}
+            </RadioGroup>
+          </FormControl>
+        </Paper>
+        <Paper sx={{ mb: 2, p: 2 }}>
+          {types.map((type) => (
+            <FormGroup>
+              <FormControlLabel
+                control={<Checkbox />}
+                label={type}
+                key={type}
+              />
+            </FormGroup>
+          ))}
+        </Paper>
+
+        <Paper sx={{ mb: 2, p: 2 }}>
+          {brands.map((brand) => (
+            <FormGroup>
+              <FormControlLabel
+                control={<Checkbox />}
+                label={brand}
+                key={brand}
+              />
+            </FormGroup>
+          ))}
+        </Paper>
+      </Grid>
+      <Grid item xs={8} sm={8} md={9} lg={8} xl={9}>
+        <ProductList products={products} />
+      </Grid>
+      <Grid item xs={3}></Grid>
+      <Grid item xs={9}>
+        <Box display="flex" justifyContent="space-between" alignItems="center">
+          <Pagination color="primary" count={10} shape="rounded" />
+          <Typography> Showing 1-6 of 20</Typography>
+        </Box>
+      </Grid>
+    </Grid>
   );
 };
 
