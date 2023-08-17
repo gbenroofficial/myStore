e5ac29c8c103746ac014fbffaffcfde6d67f3c37
gbenroofficial <68187993+gbenroofficial@users.noreply.github.com>
2023-08-17 13:51:09 +0100

    productSearch component & debounced

---
+++ store_client/src/features/catalogue/ProductSearch.tsx
@@ -0,0 +1,27 @@
+import { TextField, debounce } from "@mui/material";
+import React, { useState } from "react";
+import { useAppDispatch, useAppSelector } from "../../App/store/configureStore";
+import { setProductParams } from "./catalogueSlice";
+
+const ProductSearch = () => {
+  const { productParams } = useAppSelector((state) => state.catalogue);
+  const dispatch = useAppDispatch();
+  const [searchTerm, setSearchTerm] = useState(productParams.searchTerm);
+  const debouncedSearch = debounce((event: any)=>{
+    dispatch(setProductParams({ searchTerm: event.target.value }));
+  }, 1500);
+  return (
+    <TextField
+      label="Search products"
+      variant="outlined"
+      fullWidth
+      value={searchTerm || ""}
+      onChange={(event:any) => {
+        setSearchTerm(event.target.value);
+        debouncedSearch(event);
+      }}
+    />
+  );
+};
+
+export default ProductSearch;
