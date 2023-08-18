d0b540edd6ca44e964b6a302086abb77cbf3d19f
gbenroofficial <68187993+gbenroofficial@users.noreply.github.com>
2023-08-18 21:17:15 +0100

    logic implementation of checkBoxset

--- store_client/src/features/components/CheckBoxSet.tsx
+++ store_client/src/features/components/CheckBoxSet.tsx
@@ -1,16 +1,33 @@
 import { FormGroup, FormControlLabel, Checkbox } from "@mui/material";
+import { useState } from "react";
 
 interface Props {
   items: string[];
   checked?: string[];
   onChange: (items: string[]) => void;
 }
-const CheckBoxSet = ({items, checked, onChange}: Props) => {
+const CheckBoxSet = ({ items, checked, onChange }: Props) => {
+  const [checkedItems, setCheckedItems] = useState(checked || []);
+
+  function handleChecked(val: string) {
+    var currentIndex = checkedItems.findIndex((item) => item === val);
+    let newCheckedItems = [];
+
+    if (currentIndex === -1) newCheckedItems = [...checkedItems, val];
+    else newCheckedItems = checkedItems.filter((item) => item !== val);
+
+    setCheckedItems(newCheckedItems);
+    onChange(newCheckedItems);
+  }
   return (
     <>
       <FormGroup>
         {items.map((item) => (
-          <FormControlLabel control={<Checkbox />} label={item} key={item} />
+          <FormControlLabel
+            control={<Checkbox checked={checkedItems.indexOf(item) !== -1} onClick={() => {handleChecked(item)}}/>}
+            label={item}
+            key={item}
+          />
         ))}
       </FormGroup>
     </>
