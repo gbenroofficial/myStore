35a6d69bec936392524462e9a08a34a04b76c305
gbenroofficial <68187993+gbenroofficial@users.noreply.github.com>
2023-08-26 23:09:15 +0100

    configure swaggr with token

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,13 @@
+using System.Text;
 using API.Data;
 using API.Entities;
 using API.Middleware;
 using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,12 +18,50 @@ builder.Services.AddDbContext<StoreContext>(opt => {
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen( c => 
+{
+    var jwtsecurityScheme = new OpenApiSecurityScheme
+    {
+        BearerFormat = "JWT",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = JwtBearerDefaults.AuthenticationScheme,
+        Description = "Put Bearer + your token in box below",
+        Reference = new OpenApiReference
+        {
+            Id = JwtBearerDefaults.AuthenticationScheme,
+            Type = ReferenceType.SecurityScheme
+        }
+    };
+
+    c.AddSecurityDefinition(jwtsecurityScheme.Reference.Id, jwtsecurityScheme);
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {jwtsecurityScheme, Array.Empty<string>()}
+    });
+});
+
+
+
 builder.Services.AddCors();
 builder.Services.AddIdentityCore<User>(opt => {opt.User.RequireUniqueEmail = true;})
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<StoreContext>();
-builder.Services.AddAuthentication();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(opt => 
+                
+                    opt.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWTSettings:TokenKey"]))
+                    }
+                );
+
 builder.Services.AddAuthorization();
 builder.Services.AddScoped<TokenService>();
 
@@ -32,7 +74,10 @@ app.UseMiddleware<ExceptionMiddleware>();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.ConfigObject.AdditionalItems.Add("persistAuthorization", "true");
+    });
 }
 app.UseCors(
     opt => {
@@ -42,9 +87,9 @@ app.UseCors(
         .WithOrigins("http://localhost:3000")
         .SetPreflightMaxAge(TimeSpan.FromMinutes(10)); // Adjust the max age as needed
 });
-    
-app.UseAuthorization();
 
+app.UseAuthentication();    
+app.UseAuthorization();
 app.MapControllers();
 
 var scope = app.Services.CreateScope();
