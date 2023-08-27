eba8d0ed6e951333773333c02441286ac94b8a2c
gbenroofficial <68187993+gbenroofficial@users.noreply.github.com>
2023-08-27 16:16:33 +0100

    make login page

--- store_client/src/features/account/Login.tsx
+++ store_client/src/features/account/Login.tsx
@@ -1,9 +1,99 @@
-const Login = () => {
-  return (
-    <>
-      <h1> Login Page</h1>
-    </>
-  );
-};
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Paper } from "@mui/material";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import agent from "../../App/api/agent";
 
-export default Login;
+// TODO remove, this demo shouldn't need to reset the theme.
+const defaultTheme = createTheme();
+
+export default function Login() {
+  const [values, setValues] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    console.log(values);
+
+    agent.Account.login(values)
+      .then((result) => console.log(result))
+      .catch(() => {});
+  };
+
+  const handleInputChange = (event: any) => {
+    const { name, value } = event.target;
+    setValues({ ...values, [name]: value });
+  };
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Container
+        component={Paper}
+        maxWidth="xs"
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+          p: 4,
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            fullWidth
+            label="Username"
+            name="username"
+            autoFocus
+            onChange={handleInputChange}
+            value={values.username}
+          />
+          <TextField
+            margin="normal"
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            onChange={handleInputChange}
+            value={values.password}
+          />
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item>
+              <Link to="/register">{"Don't have an account? Sign Up"}</Link>
+            </Grid>
+            <Grid item xs>
+              {/* <Link href="#" variant="body2">
+                Forgot password?
+              </Link> */}
+            </Grid>
+          </Grid>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}
