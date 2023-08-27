df7bcf1da6c3985821e3f652086ad0910862ec2c
gbenroofficial <68187993+gbenroofficial@users.noreply.github.com>
2023-08-27 20:56:52 +0100

    error catch

--- store_client/src/App/api/agent.ts
+++ store_client/src/App/api/agent.ts
@@ -67,10 +67,10 @@ axios.interceptors.response.use(
 
 const requests = {
   get: (url: string, params?: URLSearchParams) =>
-    axios.get(url, { params }).then(responseBody),
-  post: (url: string, body: {}) => axios.post(url, body).then(responseBody),
-  put: (url: string, body: {}) => axios.put(url, body).then(responseBody),
-  delete: (url: string) => axios.delete(url).then(responseBody),
+    axios.get(url, { params }).then(responseBody).catch(()=>{}),
+  post: (url: string, body: {}) => axios.post(url, body).then(responseBody).catch(()=>{}),
+  put: (url: string, body: {}) => axios.put(url, body).then(responseBody).catch(()=>{}),
+  delete: (url: string) => axios.delete(url).then(responseBody).catch(()=>{}),
 };
 
 const Catalogue = {
