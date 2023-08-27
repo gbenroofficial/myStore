f93e3d96097cb23b93a8d25b64dd23d19d008006
gbenroofficial <68187993+gbenroofficial@users.noreply.github.com>
2023-08-27 21:28:03 +0100

    add form validation

--- store_client/src/features/account/Login.tsx
+++ store_client/src/features/account/Login.tsx
@@ -1,98 +1,98 @@
-import * as React from "react";
 import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { Paper } from "@mui/material";
 import { Link } from "react-router-dom";
-import { useState } from "react";
 import agent from "../../App/api/agent";
-
-// TODO remove, this demo shouldn't need to reset the theme.
-const defaultTheme = createTheme();
+import { FieldValues, useForm } from "react-hook-form";
+import { LoadingButton } from "@mui/lab";
 
 export default function Login() {
-  const [values, setValues] = useState({
-    username: "",
-    password: "",
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, errors, isValid },
+  } = useForm({
+    mode: "onTouched",
   });
 
-  const handleSubmit = (event: any) => {
-    event.preventDefault();
-    
-    agent.Account.login(values)
-      .then(() => {})
-      .catch(() => {});
-  };
-
-  const handleInputChange = (event: any) => {
-    const { name, value } = event.target;
-    setValues({ ...values, [name]: value });
-  };
+  async function submitForm(data: FieldValues) {
+    try {
+      await agent.Account.login(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Container
-        component={Paper}
-        maxWidth="xs"
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          flexDirection: "column",
-          p: 4,
-        }}
+    <Container
+      component={Paper}
+      maxWidth="xs"
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+        p: 4,
+      }}
+    >
+      <Avatar sx={{ m: 1, bgcolor: "primary.main" }}>
+        <LockOutlinedIcon />
+      </Avatar>
+      <Typography component="h1" variant="h5">
+        Sign in
+      </Typography>
+      <Box
+        component="form"
+        onSubmit={handleSubmit(submitForm)}
+        noValidate
+        sx={{ mt: 1 }}
       >
-        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-          <TextField
-            margin="normal"
-            fullWidth
-            label="Username"
-            name="username"
-            autoFocus
-            onChange={handleInputChange}
-            value={values.username}
-          />
-          <TextField
-            margin="normal"
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            onChange={handleInputChange}
-            value={values.password}
-          />
+        <TextField
+          margin="normal"
+          fullWidth
+          label="Username"
+          autoFocus
+          autoComplete="username"
+          {...register("username", { required: "username is required" })}
+          error={!!errors.username}
+          helperText={errors?.username?.message as string}
+        />
+        <TextField
+          margin="normal"
+          fullWidth
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          {...register("password", { required: "password is required" })}
+          error={!!errors.password}
+          helperText={errors?.password?.message as string}
+        />
 
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-          >
-            Sign In
-          </Button>
-          <Grid container>
-            <Grid item>
-              <Link to="/register">{"Don't have an account? Sign Up"}</Link>
-            </Grid>
-            <Grid item xs>
-              {/* <Link href="#" variant="body2">
+        <LoadingButton
+          loading={isSubmitting}
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+          disabled={!isValid}
+        >
+          Sign In
+        </LoadingButton>
+        <Grid container>
+          <Grid item>
+            <Link to="/register">{"Don't have an account? Sign Up"}</Link>
+          </Grid>
+          <Grid item xs>
+            {/* <Link href="#" variant="body2">
                 Forgot password?
               </Link> */}
-            </Grid>
           </Grid>
-        </Box>
-      </Container>
-    </ThemeProvider>
+        </Grid>
+      </Box>
+    </Container>
   );
 }
