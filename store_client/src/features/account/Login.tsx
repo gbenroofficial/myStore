01d63ec2bc594e2a0f39d16548b285ca55aef452
gbenroofficial <68187993+gbenroofficial@users.noreply.github.com>
2023-08-28 18:00:27 +0100

    add loggedIn menu

--- store_client/src/features/account/Login.tsx
+++ store_client/src/features/account/Login.tsx
@@ -26,7 +26,7 @@ export default function Login() {
 
   async function submitForm(data: FieldValues) {
     try {
-      await dispatch(signInUser({data}));
+      await dispatch(signInUser(data));
       navigate("/catalogue");
     } catch (error) {
       console.log(error);
