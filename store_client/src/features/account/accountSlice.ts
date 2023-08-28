1260f247f4ed3be6c471695d753e724632627f8a
gbenroofficial <68187993+gbenroofficial@users.noreply.github.com>
2023-08-28 21:29:31 +0100

    add toastify invalid token

--- store_client/src/features/account/accountSlice.ts
+++ store_client/src/features/account/accountSlice.ts
@@ -3,6 +3,7 @@ import { User } from "../../App/Models/User";
 import agent from "../../App/api/agent";
 import { FieldValues } from "react-hook-form";
 import { router } from "../../App/router/Router";
+import { toast } from "react-toastify";
 
 interface AccountState {
   user: User | null;
@@ -24,13 +25,30 @@ export const signInUser = createAsyncThunk<User, FieldValues>(
 );
 
 export const getCurrentUser = createAsyncThunk<User>(
-  "account/signIn",
+  "account/getCurrentUser",
   async (_, thunkAPI) => {
+    thunkAPI.dispatch(setUser(JSON.parse(localStorage.getItem("user")!)));
     try {
-      const user = agent.Account.currentUser();
-      localStorage.setItem("user", JSON.stringify(user));
-      return user;
-    } catch (error) {}
+      const user = await agent.Account.currentUser();
+      if (user) {
+        localStorage.setItem("user", JSON.stringify(user));
+        return user;
+      }
+      else {        
+        toast.error("session has expired. Please log in again");
+        thunkAPI.dispatch(setUser(null))
+        localStorage.removeItem("user");
+        router.navigate("/login");
+        
+    }
+    } catch (error: any) {
+      return thunkAPI.rejectWithValue({ error: error.data });
+    }
+  },
+  {
+    condition: () => {
+      if (!localStorage.getItem("user")) return false;
+    },
   }
 );
 
@@ -43,20 +61,26 @@ export const accountSlice = createSlice({
       localStorage.removeItem("user");
       router.navigate("/");
     },
+    setUser: (state, action) => {
+      state.user = action.payload;
+    },
   },
   extraReducers(builder) {
+    builder.addCase(getCurrentUser.rejected, (state) => {
+      state.user = null;
+      localStorage.removeItem("user");
+      toast.error("session has expired. Please log in again");
+      router.navigate("/login");
+    });
     builder.addMatcher(
       isAnyOf(signInUser.fulfilled, getCurrentUser.fulfilled),
       (state, action) => {
         state.user = action.payload;
       }
     );
-    builder.addMatcher(
-      isAnyOf(signInUser.rejected, getCurrentUser.rejected),
-      (state, action) => {
-        console.log(action.payload);
-      }
-    );
+    builder.addMatcher(isAnyOf(signInUser.rejected), (state, action) => {
+      console.log(action.payload);
+    });
   },
 });
-export const { logOut } = accountSlice.actions;
+export const { logOut, setUser } = accountSlice.actions;
