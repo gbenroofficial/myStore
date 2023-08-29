1aa2aa53875a371980fdb922ac4c4ebea38eb784
gbenroofficial <68187993+gbenroofficial@users.noreply.github.com>
2023-08-29 19:54:56 +0100

    add Register component

--- store_client/src/features/account/Register.tsx
+++ store_client/src/features/account/Register.tsx
@@ -1,9 +1,109 @@
-const Register = () => {
-  return (
-    <>
-      <h1> Register Page</h1>
-    </>
-  );
-};
+import Avatar from "@mui/material/Avatar";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { Alert, AlertTitle, List, ListItem, ListItemText, Paper } from "@mui/material";
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { LoadingButton } from "@mui/lab";
+import agent from "../../App/api/agent";
+import { useState } from "react";
 
-export default Register;
+export default function Register() {
+  const [validationErrors, setValidationErrors] = useState([]);
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, errors, isValid },
+  } = useForm({
+    mode: "onTouched",
+  });
+
+  return (
+    <Container
+      component={Paper}
+      maxWidth="xs"
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+        p: 4,
+      }}
+    >
+      <Avatar sx={{ m: 1, bgcolor: "primary.main" }}>
+        <LockOutlinedIcon />
+      </Avatar>
+      <Typography component="h1" variant="h5">
+        Register
+      </Typography>
+      <Box
+        component="form"
+        onSubmit={handleSubmit((data) => agent.Account.register(data).catch((error)=>{setValidationErrors(error)}))}
+        noValidate
+        sx={{ mt: 1 }}
+      >
+        <TextField
+          margin="normal"
+          fullWidth
+          label="Username"
+          autoFocus
+          {...register("username", { required: "username is required" })}
+          error={!!errors.username}
+          helperText={errors?.username?.message as string}
+        />
+        <TextField
+          margin="normal"
+          fullWidth
+          label="Email"
+          {...register("email", { required: "email is required" })}
+          error={!!errors.email}
+          helperText={errors?.email?.message as string}
+        />
+        <TextField
+          margin="normal"
+          fullWidth
+          label="Password"
+          type="password"
+          {...register("password", { required: "password is required" })}
+          error={!!errors.password}
+          helperText={errors?.password?.message as string}
+        />
+        {validationErrors.length > 0 && (
+        <Alert severity="error">
+          <AlertTitle> Validation Errors </AlertTitle>
+          <List>
+            {validationErrors.map((error) => (
+              <ListItem key={error}>
+                <ListItemText>{error}</ListItemText>
+              </ListItem>
+            ))}
+          </List>
+        </Alert>)}
+
+        <LoadingButton
+          loading={isSubmitting}
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+          disabled={!isValid}
+        >
+          Sign Up
+        </LoadingButton>
+        <Grid container>
+          <Grid item>
+            <Link to="/login">{"Already have an account? Log in"}</Link>
+          </Grid>
+          <Grid item xs>
+            {/* <Link href="#" variant="body2">
+                Forgot password?
+              </Link> */}
+          </Grid>
+        </Grid>
+      </Box>
+    </Container>
+  );
+}
