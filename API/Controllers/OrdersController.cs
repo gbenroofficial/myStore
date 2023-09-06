2a14c21276341eac0c00186fa8be4f3adc58c2bb
gbenroofficial <68187993+gbenroofficial@users.noreply.github.com>
2023-09-06 16:58:35 +0100

    add OrderController

--- API/Controllers/OrdersController.cs
+++ API/Controllers/OrdersController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
+using API.DTOs;
 using API.Entities.OrderComponents;
+using API.Extensions;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -36,6 +38,18 @@ namespace API.Controllers
                         .Include(o => o.OrderItems)
                         .Where(x => x.BuyerId == User.Identity.Name && x.Id == id)
                         .FirstOrDefaultAsync();
+        }
+        [HttpPost]
+        public async Task<ActionResult<Order>> CreateOrder(CreateOrderDto orderDto)
+        {
+            var basket = await _context.Baskets
+                                .FindBasket(User.Identity.Name)
+                                .FirstOrDefaultAsync();
+
+            if (basket == null) return BadRequest(new ProblemDetails {Title = "Could not locate basket"});
+
+            var items = new List<OrderItem>();
+
         }
     }
 }
