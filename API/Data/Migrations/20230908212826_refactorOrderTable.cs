920145f84197e94af961b2f68c0c9d233f19b88c
gbenroofficial <68187993+gbenroofficial@users.noreply.github.com>
2023-09-08 22:38:11 +0100

    refactor migrations

---
+++ API/Data/Migrations/20230908212826_refactorOrderTable.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class refactorOrderTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ItemOrdered_ProductId",
+                table: "OrderItem",
+                newName: "ProductOrdered_ProductId");
+
+            migrationBuilder.RenameColumn(
+                name: "ItemOrdered_PictureUrl",
+                table: "OrderItem",
+                newName: "ProductOrdered_PictureUrl");
+
+            migrationBuilder.RenameColumn(
+                name: "ItemOrdered_Name",
+                table: "OrderItem",
+                newName: "ProductOrdered_Name");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProductOrdered_ProductId",
+                table: "OrderItem",
+                newName: "ItemOrdered_ProductId");
+
+            migrationBuilder.RenameColumn(
+                name: "ProductOrdered_PictureUrl",
+                table: "OrderItem",
+                newName: "ItemOrdered_PictureUrl");
+
+            migrationBuilder.RenameColumn(
+                name: "ProductOrdered_Name",
+                table: "OrderItem",
+                newName: "ItemOrdered_Name");
+        }
+    }
+}
