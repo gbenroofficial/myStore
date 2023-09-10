1674b0a8fd7912408c8d8f2029139d8b93a52550
gbenroofficial <68187993+gbenroofficial@users.noreply.github.com>
2023-09-10 22:02:13 +0100

    refactor BasketPage

--- store_client/src/features/Basket/BasketPage.tsx
+++ store_client/src/features/Basket/BasketPage.tsx
@@ -1,25 +1,14 @@
 import {
   Button,
-  CardMedia,
-  Container,
-  FormControl,
   Grid,
-  InputLabel,
-  MenuItem,
-  Paper,
-  Select,
   SelectChangeEvent,
   Typography,
 } from "@mui/material";
-import DeleteIcon from "@mui/icons-material/Delete";
-import { BasketItem } from "../../App/Models/Basket";
-import { GridColDef, DataGrid } from "@mui/x-data-grid";
 import BasketSummary from "./BasketSummary";
-import { formatCurrency } from "../../App/util/util";
 import { Link } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../../App/store/configureStore";
-import { deleteBasketItemAsync, updateBasketItemAsync } from "./basketSlice";
-import { LoadingButton } from "@mui/lab";
+import { updateBasketItemAsync } from "./basketSlice";
+import BasketTable from "./BasketTable";
 
 const BasketPage = () => {
   const dispatch = useAppDispatch();
@@ -35,104 +24,10 @@ const BasketPage = () => {
   if (!basket)
     return <Typography variant="h3">Your basket is empty</Typography>;
 
-  const columns: GridColDef[] = [
-    {
-      field: "pictureUrl",
-      headerName: "Image",
-      width: 100,
-      renderCell: (params) => (
-        <CardMedia
-          component="img"
-          sx={{ height: "90%", width: "70%", backgroundSize: "fill" }}
-          image={params.value}
-        />
-      ),
-    },
-    { field: "name", headerName: "Product", width: 300 },
-    {
-      field: "price",
-      headerName: "Price",
-      type: "number",
-      width: 90,
-      valueGetter: (params) => formatCurrency(params.value),
-    },
-    {
-      field: "quantity",
-      headerName: "Quantity",
-      width: 200,
-
-      renderCell: (params) => (
-        <>
-          <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-            <InputLabel id="demo-simple-select-label">Age</InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              value={params.row.quantity}
-              label="Age"
-              onChange={(e) => handleQuantityChange(e, params.id as number)}
-            >
-              {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((quantity) => (
-                <MenuItem key={quantity} value={quantity}>
-                  {quantity}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-        </>
-      ),
-    },
-    {
-      field: "subtotal",
-      headerName: "Subtotal",
-      type: "number",
-      width: 100,
-      valueGetter: (params) =>
-        "\u00A3" + (params.row.price * params.row.quantity) / 100,
-    },
-
-    {
-      field: "custom",
-      headerName: "",
-      width: 250,
-      renderCell: (params) => (
-        <Container sx={{ display: "flex", justifyContent: "flex-end" }}>
-          <FormControl>
-            <LoadingButton
-              loading={
-                status === "pendingBasketRemoval" + (params.id as number)
-              }
-              aria-label="delete"
-              color="error"
-              onClick={() =>
-                dispatch(
-                  deleteBasketItemAsync({ productId: params.id as number })
-                )
-              }
-            >
-              <DeleteIcon />
-            </LoadingButton>
-          </FormControl>
-        </Container>
-      ),
-    },
-  ];
-
-  const getRowId = (item: BasketItem) => item.productId;
   return (
     <>
       <div style={{ height: 800, width: "100%" }}>
-        <Paper>
-          <DataGrid
-            rows={basket.items}
-            rowHeight={70}
-            autoHeight
-            columns={columns}
-            pageSizeOptions={[5, 10]}
-            checkboxSelection
-            getRowId={getRowId}
-          />
-        </Paper>
+        <BasketTable items={basket.items} isBasket={true} />
 
         <Grid container sx={{ mt: 1 }}>
           <Grid item xs={6} />
