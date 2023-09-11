34a3b688ea3cd5ca6338c2ca1d30ee977e04e71c
gbenroofficial <68187993+gbenroofficial@users.noreply.github.com>
2023-09-11 20:18:37 +0100

    add additional validation objects

--- store_client/src/features/checkout/checkOutValidation.ts
+++ store_client/src/features/checkout/checkOutValidation.ts
@@ -1,5 +1,6 @@
 import * as yup from "yup";
-export const validationSchema = yup.object({
+export const validationSchema = [
+  yup.object({
     fullName: yup.string().required("Full name is required"),
     address1: yup.string().required("Address is required"),
     address2: yup.string().required(),
@@ -7,5 +8,9 @@ export const validationSchema = yup.object({
     region: yup.string().required(),
     postCode: yup.string().required(),
     country: yup.string().required(),
-
-})
+  }),
+  yup.object(),
+  yup.object({
+    nameOnCard: yup.string().required(),
+  }),
+];
