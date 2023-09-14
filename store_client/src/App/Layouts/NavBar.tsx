4950c9235497934b87515264d76021c497a51363
gbenroofficial <68187993+gbenroofficial@users.noreply.github.com>
2023-09-14 21:26:49 +0100

    refactor NavBar

--- store_client/src/App/Layouts/NavBar.tsx
+++ store_client/src/App/Layouts/NavBar.tsx
@@ -1,14 +1,19 @@
 import {
   AppBar,
   Badge,
+  Box,
+  Button,
   Container,
   FormControlLabel,
   FormGroup,
   IconButton,
   List,
   ListItem,
+  Menu,
+  MenuItem,
   Switch,
   Toolbar,
+  Tooltip,
   Typography,
 } from "@mui/material";
 import React, { useState } from "react";
@@ -16,6 +21,16 @@ import { Link, NavLink } from "react-router-dom";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import { useAppSelector } from "../store/configureStore";
 import LoggedInMenu from "./LoggedInMenu";
+import MenuIcon from "@mui/icons-material/Menu";
+import LoggedOutMenu from "./LoggedOutMenu";
+
+const sectionsList = [
+  { title: "home", path: "/" },
+  { title: "catalogue", path: "/catalogue" },
+  { title: "about", path: "/about" },
+  { title: "contact", path: "/contact" },
+];
+
 interface Props {
   setDarkMode: React.Dispatch<React.SetStateAction<boolean>>;
 }
@@ -23,22 +38,29 @@ const NavBar = ({ setDarkMode }: Props) => {
   const [isChecked, setIsChecked] = useState(false);
   const [checkLabel, setCheckLabel] = useState("Dark mode");
 
+  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   function toggleCheck(event: React.ChangeEvent<HTMLInputElement>) {
     setIsChecked(event.target.checked);
     isChecked ? setDarkMode(false) : setDarkMode(true);
     isChecked ? setCheckLabel("Dark mode") : setCheckLabel("Light mode");
   }
-
-  const sectionsList = [
-    { title: "home", path: "/" },
-    { title: "catalogue", path: "/catalogue" },
-    { title: "about", path: "/about" },
-    { title: "contact", path: "/contact" },
-  ];
-  const logList = [
-    { title: "login", path: "/login" },
-    { title: "register", path: "/register" },
-  ];
 
   const { basket } = useAppSelector((state) => state.basket);
   const { user } = useAppSelector((state) => state.account);
@@ -50,74 +72,138 @@ const NavBar = ({ setDarkMode }: Props) => {
 
   return (
     <AppBar position="sticky" sx={{ mb: 4 }}>
-      <Toolbar sx={{ display: "flex" }}>
-        <Typography variant="h6">Soso</Typography>
+      <Container maxWidth="xl">
+        <Toolbar>
+          <Typography
+            variant="h6"
+            noWrap
+            component={NavLink}
+            to={"/"}
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            SOSO
+          </Typography>
 
-        <List sx={{ ml: 2, display: "flex" }}>
-          {sectionsList.map(({ title, path }) => (
-            <ListItem
-              component={NavLink}
-              to={path}
-              key={title}
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
               sx={{
-                color: "inherit",
-                "&:hover": {
-                  color: "grey.400",
-                },
-                "&:active": {
-                  color: "action.active",
-                },
+                display: { xs: "block", md: "none" },
               }}
             >
-              {title.toUpperCase()}
-            </ListItem>
-          ))}
-        </List>
-
-        <FormGroup>
-          <FormControlLabel
-            control={<Switch checked={isChecked} onChange={toggleCheck} />}
-            label={checkLabel}
-          />
-        </FormGroup>
-        <Container sx={{ display: "flex", justifyContent: "flex-end" }}>
-          <IconButton
-            size="large"
-            aria-label="show 17 new notifications"
-            color="inherit"
-            component={Link}
-            to="/basket"
-          >
-            <Badge badgeContent={itemsCount} color="error">
-              <ShoppingCartIcon />
-            </Badge>
-          </IconButton>
-          {user ? (
-            <LoggedInMenu />
-          ) : (
-            <List sx={{ ml: 2, display: "flex" }}>
-              {logList.map(({ title, path }) => (
-                <ListItem
+              {sectionsList.map(({ title, path }) => (
+                <MenuItem
+                  key={title}
                   component={NavLink}
                   to={path}
-                  key={title}
-                  sx={{
-                    color: "inherit",
-                    "&:hover": {
-                      color: "secondary.main",
-                    },
-                    "&:active": {
-                      color: "text.secondary",
-                    },
-                  }}
+                  onClick={handleCloseNavMenu}
                 >
-                  {title.toUpperCase()}
-                </ListItem>
+                  <Typography textAlign="center">
+                    {title.toUpperCase()}
+                  </Typography>
+                </MenuItem>
               ))}
-            </List>
-          )}
-        </Container>
-      </Toolbar>
+            </Menu>
+          </Box>
+
+          <Typography
+            variant="h5"
+            noWrap
+            component={NavLink}
+            to={"/"}
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            SOSO
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {sectionsList.map(({ title, path }) => (
+              <Button
+                key={title}
+                component={NavLink}
+                to={path}
+                onClick={handleCloseNavMenu}
+                sx={{
+                  color: "inherit",
+                  "&:hover": {
+                    color: "secondary.main",
+                  },
+                  "&:active": {
+                    color: "text.secondary",
+                  },
+                }}
+              >
+                {title.toUpperCase()}
+              </Button>
+            ))}
+
+            <FormControlLabel
+              control={<Switch checked={isChecked} onChange={toggleCheck} />}
+              label={checkLabel}
+              sx={{ ml: 2 }}
+            />
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <IconButton
+              size="large"
+              aria-label="show 17 new notifications"
+              color="inherit"
+              component={Link}
+              to="/basket"
+            >
+              <Badge badgeContent={itemsCount} color="error">
+                <ShoppingCartIcon />
+              </Badge>
+            </IconButton>
+
+            {user ? (
+              <LoggedInMenu />
+            ) : (
+              <LoggedOutMenu />
+            )}
+          </Box>
+        </Toolbar>
+      </Container>
     </AppBar>
   );
 };
