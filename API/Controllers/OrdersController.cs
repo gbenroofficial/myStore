f1df18a6a2e9e2c27a1e90f03f16d0e3065409af
gbenroofficial <68187993+gbenroofficial@users.noreply.github.com>
2023-09-19 19:44:27 +0100

    add paymentIntentId

--- API/Controllers/OrdersController.cs
+++ API/Controllers/OrdersController.cs
@@ -82,8 +82,8 @@ namespace API.Controllers
                 BuyerId = User.Identity.Name,
                 ShippingAddress = orderDto.ShippingAddress,
                 SubTotal = subTotal,
-                DeliveryFee = deliveryFee
-    
+                DeliveryFee = deliveryFee,
+                PaymentIntentId = basket.PaymentIntentId    
             
             };
 
